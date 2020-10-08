4294aeca91dd93098949bab6b8ad1d273d4e2b31
Suraj-ui <sp990047@gmail.com>
2020-10-08 13:21:43 +0530

    Updates

---
+++ precache-manifest.139ae5f137d0b922b79f1172aeae41d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeca6d9eadea70a42d4d059d5db382c2",
+    "url": "/demolive/index.html"
+  },
+  {
+    "revision": "7f0e40e5e66d3f5d67af",
+    "url": "/demolive/static/js/2.c4ecbe7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demolive/static/js/2.c4ecbe7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad7aabc06125ab42abe",
+    "url": "/demolive/static/js/main.0e2ad78d.chunk.js"
+  },
+  {
+    "revision": "f220d830b8b83401a477",
+    "url": "/demolive/static/js/runtime-main.cf361812.js"
+  }
+]);
