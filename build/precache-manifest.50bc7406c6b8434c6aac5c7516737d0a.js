3e8f5f4e7ef58a0e01350df7a3c477386d728214
Suraj-ui <sp990047@gmail.com>
2020-10-08 13:04:58 +0530

    first commit

---
+++ build/precache-manifest.50bc7406c6b8434c6aac5c7516737d0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e102bda6b5ccadfe4b35371566ac825a",
+    "url": "/Suraj-ui/demo.git/index.html"
+  },
+  {
+    "revision": "7f0e40e5e66d3f5d67af",
+    "url": "/Suraj-ui/demo.git/static/js/2.c4ecbe7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Suraj-ui/demo.git/static/js/2.c4ecbe7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad7aabc06125ab42abe",
+    "url": "/Suraj-ui/demo.git/static/js/main.0e2ad78d.chunk.js"
+  },
+  {
+    "revision": "454ea21fd38ce65fe51d",
+    "url": "/Suraj-ui/demo.git/static/js/runtime-main.46f04cc4.js"
+  }
+]);
